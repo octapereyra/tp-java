bd092df4de3e9c8860aa5061cf913ba515a6c8eb
tomib <tomib@LAPTOP-CDTA9PP1>
2021-09-02 23:44:56 -0300

    Se agregó clase usuario con atributos, getters y setters.

--- src/main/java/entities/Usuario.java
+++ src/main/java/entities/Usuario.java
@@ -2,6 +2,52 @@ package entities;
 
 public class Usuario {
 	
-	String apellido;
+	private int id_usuario;
+	private String nombre;
+	private String apellido;
+	private String nombreUusuario;
+	private String email;
+	public enum tipoUsuario{
+		usuario, administrador;
+	};
+	private String clave;
+	
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNombreUusuario() {
+		return nombreUusuario;
+	}
+	public void setNombreUusuario(String nombreUusuario) {
+		this.nombreUusuario = nombreUusuario;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
 
 }
